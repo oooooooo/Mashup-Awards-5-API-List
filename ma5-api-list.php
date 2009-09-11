6638dd1ed6a4b8d9b900f90fc780d740c5403ef4
oooooooo <bluecatbluedog+github@gmail.com>
2009-09-11 10:43:20 +0900

    first commit

---
+++ ma5-api-list.php
@@ -0,0 +1,70 @@
+<?php
+define('MAX_PAGE', 100);
+
+error_reporting(E_ALL);
+
+function data($page) {
+  $file = f("http://mashupaward.jp/category/3176/$page/");
+  $data = array();
+  $regex = regex();
+  if (preg_match_all("|$regex|ms", $file, $match_all, PREG_SET_ORDER)) {
+    foreach ($match_all as $match) {
+      $data[] = array(
+        'title'    => $match['title'],
+        'category' => trim_category($match['category']),
+        'guide'    => $match['guide'],
+        );
+    }
+  }
+  else {
+    return false;
+  }
+
+  return $data;
+}
+
+function f($url) {
+  for ($i = 0; $i < 3; $i++) {
+    $file = file_get_contents($url);
+    if ($file) {
+      return $file;
+    }
+  }
+  return false;
+}
+
+function regex() {
+  $open     = '<div class="entry_list_wrap">.+?<p class="thumb">.+?</p>';
+  $title    = '<h3>(?P<title>.+?)</h3>';
+  $category = '<span class="category">(?P<category>.+?)</span>';
+  $guide    = '<p>提供：(?P<guide>.+?)</p>';
+  $close    = '</div>';
+
+  $regex = "{$open}.+?{$title}.+?{$category}.+?{$guide}.+?{$close}";
+
+  return $regex;
+}
+
+function trim_category($category) {
+  return preg_replace('/\s/', '', $category);
+}
+
+function view($data) {
+  foreach ($data as $match) {
+    print <<<_HTML_
+{$match['title']}<br>
+{$match['category']}<br>
+{$match['guide']}
+<hr>
+_HTML_;
+  }
+}
+
+for ($page = 1; $page <= MAX_PAGE; $page++) {
+  if ($data = data($page)) {
+    view($data);
+  }
+  else {
+    break;
+  }
+}
