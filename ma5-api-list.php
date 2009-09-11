f5d7b18c1c394384e01d6f3107a63fb072ca23d8
oooooooo <bluecatbluedog+github@gmail.com>
2009-09-11 14:37:42 +0900

    出力バッファリング導入と関数名変更

--- ma5-api-list.php
+++ ma5-api-list.php
@@ -5,8 +5,8 @@
  * API リストを一覧にする
  * http://mashupaward.jp/
  *
- * @since      2009-09-11
- * @author     oooooooo
+ * @since  2009-09-11
+ * @author oooooooo
  */
 
 define('MAX_PAGE', 100);
@@ -24,12 +24,12 @@ set_time_limit(0);
 function data($page) {
   $file = f("http://mashupaward.jp/category/3176/$page/");
   $data = array();
-  $regex = regex();
+  $regex = getRegex();
   if (preg_match_all("|$regex|ms", $file, $match_all, PREG_SET_ORDER)) {
     foreach ($match_all as $match) {
       $data[] = array(
         'title'    => $match['title'],
-        'category' => trim_category($match['category']),
+        'category' => filterCategory($match['category']),
         'guide'    => $match['guide'],
         );
     }
@@ -43,6 +43,7 @@ function data($page) {
 
 /**
  * 粘り強い file_get_contents()
+ * - 取得に失敗しても 3 回試す
  *
  * @param  string $url
  * @return string
@@ -59,12 +60,23 @@ function f($url) {
 }
 
 /**
+ * Category を trim
+ *
+ * @param  string $category
+ * @return string
+ */
+
+function filterCategory($category) {
+  return preg_replace('/\s/', '', $category);
+}
+
+/**
  * MA5 API List を解析する正規表現
  *
  * @return string
  */
 
-function regex() {
+function getRegex() {
   $open     = '<div class="entry_list_wrap">.+?<p class="thumb">.+?</p>';
   $title    = '<h3>(?P<title>.+?)</h3>';
   $category = '<span class="category">(?P<category>.+?)</span>';
@@ -74,17 +86,6 @@ function regex() {
   $regex = "{$open}.+?{$title}.+?{$category}.+?{$guide}.+?{$close}";
 
   return $regex;
-}
-
-/**
- * Category を trim
- *
- * @param  string $category
- * @return string
- */
-
-function trim_category($category) {
-  return preg_replace('/\s/', '', $category);
 }
 
 /**
@@ -109,9 +110,12 @@ _HTML_;
  * 各ページから API を取得して出力
  */
 
+ob_start();
 for ($page = 1; $page <= MAX_PAGE; $page++) {
   if ($data = data($page)) {
     view($data);
+    ob_flush();
+    flush();
   }
   else {
     break;
