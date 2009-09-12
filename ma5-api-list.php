ba873924d67892d4dbb6eb0e288facc17d4a6857
oooooooo <obara@obara-PC.(none)>
2009-09-12 10:31:44 +0900

    カテゴリ順にする

--- ma5-api-list.php
+++ ma5-api-list.php
@@ -9,14 +9,14 @@
  * @author oooooooo
  */
 
-define('MAX_PAGE', 100);
+define('MAX_PAGE', 10);
 
 error_reporting(E_ALL);
 set_time_limit(0);
 
 /**
  * 粘り強い file_get_contents()
- * - 取得に失敗しても 3 回試す
+ * - file_get_contents() はたまに取得に失敗するので 3 回試す
  *
  * @param  string $url
  * @return string
@@ -52,22 +52,14 @@ function flushBuffers(){
  * @return array
  */
 
-function getData($page) {
+function getAPI($url, $page) {
   $data = array();
-  $regex = getRegex();
-
-  $file = f("http://mashupaward.jp/category/3176/$page/");
-  if (preg_match_all("|$regex|ms", $file, $match_all, PREG_SET_ORDER)) {
-    foreach ($match_all as $match) {
-      $data[] = array(
-        'title'    => $match['title'],
-        'category' => $match['category'],
-        'guide'    => $match['guide'],
-        );
-    }
-  }
-  else {
-    return false;
+  foreach (getAPIData($url, $page) as $api_data) {
+    $data[] = array(
+      'title'    => $api_data['title'],
+      'category' => $api_data['category'],
+      'guide'    => $api_data['guide'],
+      );
   }
 
   return $data;
@@ -79,16 +71,85 @@ function getData($page) {
  * @return string
  */
 
-function getRegex() {
-  $open     = '<div class="entry_list_wrap">.+?<p class="thumb">.+?</p>';
-  $title    = '<h3>(?P<title>.+?)</h3>';
-  $category = '<span class="category">(?P<category>.+?)</span>';
-  $guide    = '<p>提供：(?P<guide>.+?)</p>';
-  $close    = '</div>';
+function getAPIData($url, $page) {
+  $file = f("$url$page");
+  
+  $regex_open     = '<div class="entry_list_wrap">.+?<p class="thumb">.+?</p>';
+  $regex_title    = '<h3>(?P<title>.+?)</h3>';
+  $regex_category = '<span class="category">(?P<category>.+?)</span>';
+  $regex_guide    = '<p>提供：(?P<guide>.+?)</p>';
+  $regex_close    = '</div>';
 
-  $regex = "{$open}.+?{$title}.+?{$category}.+?{$guide}.+?{$close}";
+  $regex = "{$regex_open}.+?{$regex_title}.+?{$regex_category}.+?{$regex_guide}.+?{$regex_close}";
 
-  return $regex;
+  preg_match_all("|$regex|ms", $file, $match_all, PREG_SET_ORDER);  
+
+  return $match_all;
+}
+
+/**
+ * カテゴリ ( url と category_name の配列 ) を返す
+ *
+ * @return array
+ */
+
+function getCategory() {
+  $category_raw_data     = getCategoryRawData();
+  $category_url_and_name = getCategoryUrlAndName($category_raw_data);
+  return $category_url_and_name;
+}
+
+/**
+ * ページからカテゴリの生データ取得
+ *
+ * @return string
+ */
+
+function getCategoryRawData() {
+  $file = f('http://mashupaward.jp/');
+  
+  $regex_open1 = '<a class="plugin_show_category_side_ajax_first_category" href="javascript:void\(0\)">API一覧';
+  $regex_open2 = '<ul class="plugin_show_category_side_ajax_second_categories">';
+  $regex_open  = "{$regex_open1}.+?{$regex_open2}";
+  $regex_close = '</ul>';
+
+  $data = '';
+  if (preg_match("|{$regex_open}.+?(?<url_and_category_name>.+?){$regex_close}|ms", $file, $match)) {
+    $data = $match['url_and_category_name'];
+  }
+
+  return $data;
+}
+
+/**
+ * カテゴリの生データからカテゴリ名と URL 取得
+ *
+ * @param  string $category_raw_data
+ * @return array
+ */
+
+function getCategoryUrlAndName($category_raw_data) {
+  $regex_url_and_name = '<a href="(?<url>.+?)">(?<category_name>.+?)</a>';
+
+  $data = array();
+  if (preg_match_all("|$regex_url_and_name|ms", $category_raw_data, $match_all, PREG_SET_ORDER)) {
+    foreach ($match_all as $match) {
+      $data[$match['url']] = $match['category_name'];
+    }
+  }
+  
+  return $data;
+}
+
+/**
+ * フラグメント
+ *
+ * @param  string  $name
+ * @return string
+ */
+
+function getFragment($name) {
+  return urlencode($name);
 }
 
 /**
@@ -109,6 +170,24 @@ function printBody($data) {
 _HTML_;
     flushBuffers();
   }
+}
+
+/**
+ * カテゴリ名
+ *
+ * @param  string $category_name
+ * @return void
+ */
+
+function printCategoryName($category_name) {
+  $fragment = getFragment($category_name);
+  print <<<_HTML_
+<h2><a name="$fragment">$category_name</a></h2>
+
+<p><a href="#top">↑ top</a></p>
+
+_HTML_;
+  flushBuffers();
 }
 
 /**
@@ -145,9 +224,30 @@ function printHeader() {
     <title>$title</title>
   </head>
   <body>
-    <h1>$title</h1>
-    <h2>ページ送りが面倒なあなたに</h2>
+    <h1><a name="top">$title</a></h1>
+
 _HTML_;
+  flushBuffers();
+}
+
+/**
+ * メニュー
+ *
+ * @return void
+ */
+
+function printMenu($category) {
+  $data = array();
+  foreach ($category as $url=>$category_name) {
+    $fragment = getFragment($category_name);
+    $data[] = <<<_HTML_
+<a href="#$fragment">$category_name</a>
+
+_HTML_;
+
+  }
+
+  print implode(' | ', $data);
   flushBuffers();
 }
 
@@ -155,14 +255,21 @@ _HTML_;
  * 各ページから API を取得して出力
  */
 
-printHeader();
+$category = getCategory();
 
-for ($page = 1; $page <= MAX_PAGE; $page++) {
-  if ($data = getData($page)) {
-    printBody($data);
-  }
-  else {
-    break;
+printHeader();
+printMenu($category);
+
+foreach ($category as $url=>$category_name) {
+  printCategoryName($category_name);
+  
+  for ($page = 1; $page <= MAX_PAGE; $page++) {
+    if ($data = getAPI($url, $page)) {
+      printBody($data);
+    }
+    else {
+      break;
+    }
   }
 }
 
